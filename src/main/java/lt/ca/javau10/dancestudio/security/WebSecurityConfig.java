5337f17f3163dc22c89839946bd6755485f21896
karolina-andrulyte <karolina.andrulyte@gmail.com>
2024-10-14 15:24:54 +0300

    CORS added to WebSecurityConfig and now Board sections are working

--- src/main/java/lt/ca/javau10/dancestudio/security/WebSecurityConfig.java
+++ src/main/java/lt/ca/javau10/dancestudio/security/WebSecurityConfig.java
@@ -1,5 +1,9 @@
 package lt.ca.javau10.dancestudio.security;
 
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -57,6 +61,14 @@ public class WebSecurityConfig {
 	 @Bean
 	 SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 	     http.csrf(csrf -> csrf.disable())
+	     .cors(cors -> cors.configurationSource(request -> {
+             CorsConfiguration configuration = new CorsConfiguration();
+             configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+             configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+             configuration.setAllowCredentials(true);
+             configuration.setAllowedHeaders(List.of("*"));
+             return configuration;
+         }))
 	         .authorizeHttpRequests(auth -> 
 	             auth.requestMatchers("/api/auth/**").permitAll()
 	                 .requestMatchers("/api/users/**").hasAuthority("ROLE_ADMIN")
